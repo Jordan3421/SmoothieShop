d3c6beb8c3298d9264963d3dc08ec294e012ebd6
peterlitman <73899474+peterlitman@users.noreply.github.com>
2021-03-16 15:02:31 +0000

    Update CustomSmoothie.py
    Improved variable names

--- src/Entities/CustomSmoothie.py
+++ src/Entities/CustomSmoothie.py
@@ -3,13 +3,13 @@ class CustomSmoothie():
     #Initialises a CustomSmoothie instance. We pass a list of ingredients created by the user in.
     #We limit the maximum number of ingredients using the if statement. This wouldnt be necessary if this was enforced in the smoothie creation module
     #How do we restrict this paramater to only take a list?
-    def __init__(self, ingredients):
-        if len(ingredients) < 6:
-            self.ingredients = ingredients
+    def __init__(self, customIngredients):
+        if len(customIngredients) < 6:
+            self.customIngredients = customIngredients
 
     #Returns a string describing the CustomSmoothie. This string can be appended to the customer's Order/Receipt
     def describeCustomSmoothie(self):
-        ingredientsList = ", ".join(self.ingredients)
+        ingredientsList = ", ".join(self.customIngredients)
         return f"Custom Smoothie: {ingredientsList}"
 
 #Example of creating a CustomSmoothie instance, ingredient are passed in as a list.
